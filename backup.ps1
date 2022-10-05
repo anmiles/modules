56078f6e2d38ad24adeb82bdd45885d05890564a
Anatoliy Oblaukhov <mail@anmiles.net>
2022-10-05 20:54:35 +0300

    Adjust backup and restore to cross-AWS usage

--- backup.ps1
+++ backup.ps1
@@ -11,55 +11,76 @@ Param (
     [Parameter(Mandatory = $true)][string]$userdata_directory,
     [Parameter(Mandatory = $true)][string]$bucket_backup,
     [Parameter(Mandatory = $true)][string]$bucket_revision,
-    [Parameter(Mandatory = $true)][string]$environment_name,
-    [string]$backup_filename,
+    [Parameter(Mandatory = $true)][string]$state,
     [string]$snapshot_name,
-    [switch]$progress
+    [switch]$progress,
+    [switch]$quiet = $true
 )
 
-$quiet = switch($progress){$true{""} default{"--quiet"}}
+if ($quiet) { $quiet = "--quiet" } else { $quiet = "" }
 
 Import-Module $env:MODULES_ROOT\sql.ps1 -Force
 
-if ($winrm_endpoint) {
-    if (!$snapshot_name) {
-        $snapshot_name = (Get-Date).ToUniversalTime().ToString("yyyy.MM.dd_HH.mm.ss")
-    }
+if ($progress) {
+    Import-Module $env:MODULES_ROOT\timer.ps1 -Force
+    $timer = Start-Timer
+}
 
-    Write-Host "Connecting to $winrm_endpoint ..."
+Function Invoke-Command-In-Session ([object]$session, [ScriptBlock]$ScriptBlock, [Object[]]$ArgumentList) {
+    if ($session) {
+        Invoke-Command -Session $session -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList
+    } else {
+        Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList
+    }
+}
+
+$environment_name = "default"
+
+if ($state -match "\.") {
+    $arr = $state -split "\."
+    $state = $arr[0]
+    $environment_name = $arr[1]
+}
+
+if ($state -eq "dev") {
+    $session = $null
+} else {
+    $timer.StartTask("Connecting to $winrm_endpoint")
     $securePassword =  $(ConvertTo-SecureString -AsPlainText -Force $win_password)
     $credential = $(New-Object System.Management.Automation.PSCredential $win_username, $securePassword)
     $session = New-PSSession -ConnectionUri $winrm_endpoint -Credential $credential
-
-    Write-Host "Compressing userdata in $userdata_directory ..."
-    Invoke-Command -Session $session -ScriptBlock {
-        param($userdata_directory)
-        md $userdata_directory -Force
-        cd $userdata_directory
-        & 'C:\Program Files\7-Zip\7z.exe' a -y -r -bso0 -bsp0 -mx=0 "$userdata_directory.zip" .
-    } -ArgumentList $userdata_directory
-
-    Write-Host "Uploading userdata to snapshot $snapshot_name ..."
-    Invoke-Command -Session $session -ScriptBlock {
-        param($userdata_directory, $bucket_backup, $snapshot_name, $quiet)
-        aws s3 cp "$userdata_directory.zip" s3://$bucket_backup/$snapshot_name.zip $quiet
-        del -Force "$userdata_directory.zip"
-    } -ArgumentList $userdata_directory, $bucket_backup, $snapshot_name, $quiet
-
-    Write-Host "Backup database $db_name into $snapshot_name.bak ..."
-    rds_backup_database -sql_endpoint $sql_endpoint -sql_username $sql_username -sql_password $sql_password -db_name $db_name -bucket_backup $bucket_backup -snapshot_name "$snapshot_name.bak"
-
-    Write-Host "Copying JSON $environment_name.json into $snapshot_name.json ..."
-    aws s3 cp s3://$bucket_revision/$environment_name.json s3://$bucket_backup/$snapshot_name.json $quiet
-
-    Write-Host "Disconnecting..."
-    Remove-PSSession -Session $session
-} else {
-    if (!$backup_filename) { $backup_filename = "$db_name.bak" }
-    $backup_path = "$backup_directory\$backup_filename"
-
-    Write-Host "Backup database $db_name into $backup_path..."
-    sql_backup_database -sql_endpoint $sql_endpoint -sql_username $sql_username -sql_password $sql_password -db_name $db_name -filename $backup_path
 }
 
-Write-Host "Done!"
+if (!$snapshot_name) {
+    $snapshot_name = (Get-Date).ToUniversalTime().ToString("yyyy.MM.dd_HH.mm.ss")
+}
+
+$timer.StartTask("Compressing userdata in $userdata_directory")
+Invoke-Command-In-Session -Session $session -ScriptBlock {
+    param($userdata_directory)
+    md $userdata_directory -Force
+    cd $userdata_directory
+    & 'C:\Program Files\7-Zip\7z.exe' a -y -r -bso0 -bsp0 -mx=0 "$userdata_directory.zip" .
+} -ArgumentList $userdata_directory
+
+$timer.StartTask("Uploading userdata to snapshot $snapshot_name")
+Invoke-Command-In-Session -Session $session -ScriptBlock {
+    param($userdata_directory, $bucket_backup, $snapshot_name, $quiet)
+    aws s3 cp "$userdata_directory.zip" s3://$bucket_backup/$snapshot_name.zip $quiet
+    del -Force "$userdata_directory.zip"
+} -ArgumentList $userdata_directory, $bucket_backup, $snapshot_name, $quiet
+
+$timer.StartTask("Backup database $db_name into $snapshot_name.bak")
+rds_backup_database -sql_endpoint $sql_endpoint -sql_username $sql_username -sql_password $sql_password -db_name $db_name -bucket_backup $bucket_backup -snapshot_name "$snapshot_name.bak"
+
+if ($state -eq "web") {
+    $timer.StartTask("Copying JSON $environment_name.json into $snapshot_name.json")
+    aws s3 cp s3://$bucket_revision/$environment_name.json s3://$bucket_backup/$snapshot_name.json $quiet
+}
+
+if ($session) {
+    $timer.StartTask("Disconnecting")
+    Remove-PSSession -Session $session
+}
+
+$timer.Finish()
--- restore.ps1
+++ restore.ps1
@@ -10,39 +10,18 @@ Param (
     [Parameter(Mandatory = $true)][string]$db_name,
     [Parameter(Mandatory = $true)][string]$userdata_directory,
     [Parameter(Mandatory = $true)][string]$bucket_backup,
-    [string]$backup_directory,
-    [string]$backup_filename,
     [string]$snapshot_name,
-    [switch]$local,
-    [switch]$remote,
     [switch]$progress,
-    [switch]$skip_database,
-    [switch]$skip_userdata
+    [switch]$quiet = $true
 )
 
-"progress was $proress"
-
-$quiet = switch($progress){$true{""} default{"--quiet"}}
-
-"quiet is $quiet"
+if ($quiet) { $quiet = "--quiet" } else { $quiet = "" }
 
 Import-Module $env:MODULES_ROOT\sql.ps1 -Force
 
-if (!$winrm_endpoint) {
-    if (!$backup_filename) { $backup_filename = "$db_name.bak" }
-    $backup_path = "$backup_directory\$backup_filename"
-
-    if (Test-Path $backup_path) {
-        $created_time = (Get-Item $backup_path).CreationTime
-
-        if ($local -and $remote) {
-            throw "Cannot set local and remote switches at the same time"
-        }
-
-        if (!$local -and !$remote) {
-            $local = confirm "Do you want to just restore local backup that was created at ({{$created_time}})"
-        }
-    }
+if ($progress) {
+    Import-Module $env:MODULES_ROOT\timer.ps1 -Force
+    $timer = Start-Timer
 }
 
 Function Invoke-Command-In-Session ([object]$session, [ScriptBlock]$ScriptBlock, [Object[]]$ArgumentList) {
@@ -53,71 +32,59 @@ Function Invoke-Command-In-Session ([object]$session, [ScriptBlock]$ScriptBlock,
     }
 }
 
+$environment_name = "default"
+
+if ($state -match "\.") {
+    $arr = $state -split "\."
+    $state = $arr[0]
+    $environment_name = $arr[1]
+}
+
+if ($state -eq "dev") {
+    $session = $null
+} else {
+    $timer.StartTask("Connecting to $winrm_endpoint")
+    $securePassword =  $(ConvertTo-SecureString -AsPlainText -Force $win_password)
+    $credential = $(New-Object System.Management.Automation.PSCredential $win_username, $securePassword)
+    $session = New-PSSession -ConnectionUri $winrm_endpoint -Credential $credential
+}
+
 if (!$snapshot_name) {
-    $snapshot_name = $(aws s3 ls s3://$bucket_backup/) | where {$_ -match ".json"} | % {$_ -replace ".*\s(\S+)\s*?$", '$1'} | sort | select -last 1
-    $snapshot_name = $snapshot_name -replace ".json", ""
+    $timer.StartTask("Getting recent snapshot")
+    $snapshots = $(aws s3 ls s3://$bucket_backup/) | ? {$_ -match ".json"} | % {$_ -replace ".*\s(\S+)\s*?$", '$1'}
+    $snapshot = $snapshots | sort | select -last 1
+    $snapshot_name = $snapshot -replace ".json", ""
 }
 
-if (!$skip_userdata -and !$local) {
-    if ($winrm_endpoint) {
-        Write-Host "Connecting to $winrm_endpoint ..."
-        $securePassword =  $(ConvertTo-SecureString -AsPlainText -Force $win_password)
-        $credential = $(New-Object System.Management.Automation.PSCredential $win_username, $securePassword)
-        $session = New-PSSession -ConnectionUri $winrm_endpoint -Credential $credential
-    }
+$timer.StartTask("Downloading userdata from snapshot $snapshot_name into $userdata_directory")
+Invoke-Command-In-Session -Session $session -ScriptBlock {
+    param($userdata_directory, $bucket_backup, $snapshot_name, $quiet)
+    aws s3 cp s3://$bucket_backup/$snapshot_name.zip "$userdata_directory.zip" $quiet
+    Get-Item "$userdata_directory.zip" | Out-Host
+} -ArgumentList $userdata_directory, $bucket_backup, $snapshot_name, $quiet
 
-    Write-Host "Downloading userdata from snapshot $snapshot_name into $userdata_directory..."
-    Invoke-Command-In-Session -Session $session -ScriptBlock {
-        param($userdata_directory, $bucket_backup, $snapshot_name, $quiet)
-        Write-Host "Into $userdata_directory.zip"
-        aws s3 cp s3://$bucket_backup/$snapshot_name.zip "$userdata_directory.zip" $quiet
-        "Get-Item `"$userdata_directory.zip`" | Out-Host"
-        Get-Item "$userdata_directory.zip" | Out-Host
-        Write-Host "done!"
-    } -ArgumentList $userdata_directory, $bucket_backup, $snapshot_name, $quiet
+$timer.StartTask("Decompressing userdata in $userdata_directory")
+Invoke-Command-In-Session -Session $session -ScriptBlock {
+    param($userdata_directory)
+    md $userdata_directory -Force
+    cd $userdata_directory
+    del $userdata_directory\* -Recurse -Force
+    & 'C:\Program Files\7-Zip\7z.exe' x -y -r -bso0 -bsp0 "$userdata_directory.zip"
+    del -Force "$userdata_directory.zip"
+} -ArgumentList $userdata_directory
 
-    Write-Host "Decompressing userdata in $userdata_directory ..."
-    Invoke-Command-In-Session -Session $session -ScriptBlock {
-        param($userdata_directory)
-        Write-Host "Create directory $userdata_directory"
-        md $userdata_directory -Force
-        Write-Host "Go to directory $userdata_directory"
-        cd $userdata_directory
-        Write-Host "Delete all children from directory $userdata_directory"
-        del $userdata_directory\* -Recurse -Force
-        Write-Host "Start 7z "
-        & 'C:\Program Files\7-Zip\7z.exe' x -y -r -bso0 -bsp0 "$userdata_directory.zip"
-        Write-Host "delete zip"
-        del -Force "$userdata_directory.zip"
-        Write-Host "done!"
-    } -ArgumentList $userdata_directory
-}
+$timer.StartTask("Delete database $db_name")
+sql_delete_database -sql_endpoint $sql_endpoint -sql_username $sql_username -sql_password $sql_password -db_name $db_name
 
-if (!$skip_database) {
-    if ($winrm_endpoint) {
-        Write-Host "Delete database $db_name..."
-        sql_delete_database -sql_endpoint $sql_endpoint -sql_username $sql_username -sql_password $sql_password -db_name $db_name
-        
-        Write-Host "Restore database $db_name from $snapshot_name.bak ..."
-        rds_restore_database -sql_endpoint $sql_endpoint -sql_username $sql_username -sql_password $sql_password -db_name $db_name -db_username $db_username -db_password $db_password -bucket_backup $bucket_backup -snapshot_name "$snapshot_name.bak"
-    } else {
-        if (!$local) {
-            Write-Host "Downloading database from snapshot $snapshot_name into $backup_path ..."
-            aws s3 cp s3://$bucket_backup/$snapshot_name.bak $backup_path $quiet
-        }
+$timer.StartTask("Restore database $db_name from $snapshot_name.bak")
+rds_restore_database -sql_endpoint $sql_endpoint -sql_username $sql_username -sql_password $sql_password -db_name $db_name -db_username $db_username -db_password $db_password -bucket_backup $bucket_backup -snapshot_name "$snapshot_name.bak"
 
-        Write-Host "Delete database $db_name ..."
-        sql_delete_database -sql_endpoint $sql_endpoint -sql_username $sql_username -sql_password $sql_password -db_name $db_name
-        
-        Write-Host "Restore database $db_name from $backup_path ..."
-        sql_restore_database -sql_endpoint $sql_endpoint -sql_username $sql_username -sql_password $sql_password -db_name $db_name -db_username $db_username -db_password $db_password -filename $backup_path
-    }
-}
-
-Write-Host "Recycling app pools..."
+$timer.StartTask("Recycling app pools")
 Invoke-Command-In-Session -Session $session -ScriptBlock { Get-ChildItem "IIS:\AppPools\" | Restart-WebAppPool }
 
-Write-Host "Disconnecting..."
-if ($session) { Remove-PSSession -Session $session }
+if ($session) {
+    $timer.StartTask("Disconnecting")
+    Remove-PSSession -Session $session
+}
 
-Write-Host "Done!"
+$timer.Finish()
