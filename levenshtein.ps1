83f54519ea84161af50dbf133f63e770d86fe108
Anatoliy Oblaukhov <mail@anmiles.net>
2022-11-09 00:00:15 +0300

    levenshtein

---
+++ levenshtein.ps1
@@ -0,0 +1,43 @@
+Function GetTextDistance($text1, $text2) {
+	$len1 = $text1.Length;
+	$len2 = $text2.Length;
+
+	if ($len1 -eq 0) { return $len2 }
+	if ($len2 -eq 0) { return $len1 }
+
+	$diff = New-Object 'int[,]' $($len1 + 1), $($len2 + 1)
+
+	0..$len1 | % { $diff[$_, 0] = $_ }
+	0..$len2 | % { $diff[0, $_] = $_ }
+
+	1..$len1 | % {
+		$x = $_
+
+		1..$len2 | % {
+			$y = $_
+			$result = 1 - ($text2[$y - 1] -eq $text1[$x - 1])
+
+			$values = @(
+				($diff[($x - 1), $y] + 1),
+				($diff[$x, ($y - 1)] + 1),
+				($diff[($x - 1), ($y - 1)] + $result)
+			)
+
+			$diff[$x, $y] = ($values | Measure -Minimum).Minimum
+		}
+	}
+
+	return $diff[$len1, $len2]
+}
+
+Function GetClosest($text, $variants) {
+	$distances = @{}
+
+	$variants | % {
+		$distance = GetTextDistance $_ $text
+		if (!$distances[$distance]) { $distances[$distance] = @() }
+		$distances[$distance] += $_
+	}
+
+	return $distances[($distances.Keys | Sort | Select -First 1)]
+}
